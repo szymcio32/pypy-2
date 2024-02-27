ddf2a9695fe1aa7ce6c7a8f72835c0db32032605
szymon <wolnyszym@gmail.com>
2024-02-27 16:37:50 +0100

    files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="szymcio322",
+    version="0.1.0",
+    description="A simple Python package for testing",
+    packages=find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
